c28e4c090c55db25f1886d25235e2e0e65258a50
ryudain05 <dkdlsekdls@naver.com>
2025-12-09 11:03:47 +0900

    feat: Function view -> Class-based view change

--- ch2/hacker_news/post/urls.py
+++ ch2/hacker_news/post/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from post import views
 
 urlpatterns = [
-    path("", views.posts_view, name="posts"), # 전체게시글 목록
-    path("create/", views.post_create_view, name="post_create"), #  게시글 작성
-    path("<int:post_id>/", views.post_view, name="post"), #  상세 게시글 조회
-    path("<int:post_id>/like/", views.post_like_view, name="post_like"), # 게시글 좋아요
+    path("", views.PostListView.as_view(), name="posts"), # 전체게시글 목록
+    path("create/", views.PostCreateView.as_view(), name="post_create"), #  게시글 작성
+    path("<int:post_id>/", views.PostDetailView.as_view(), name="post"), #  상세 게시글 조회
+    path("<int:post_id>/like/", views.PostLikeView.as_view(), name="post_like"), # 게시글 좋아요
 ]
--- ch2/hacker_news/post/views.py
+++ ch2/hacker_news/post/views.py
@@ -2,20 +2,22 @@ from gc import get_objects
 
 from django.shortcuts import render, get_object_or_404, redirect
 from django.db.models import F
+from django.views import View
+
 from post.models import Post
 from post.forms import PostForm
 
-def posts_view(request):
-    if request.method == "GET":
+class PostListView(View):
+    def get(self, request):
         posts = Post.objects.all()
-        context = {"posts": posts, "form": PostForm}
+        context = {"posts": posts, "form": PostForm()}
         return render(request, "post_list.html", context)
 
-def post_create_view(request):
-    if request.method == "GET":
-        context = {"form": PostForm}
-        return render(request, "post_create.html", context)
-    elif request.method == "POST":
+class PostCreateView(View):
+    def get(self, request):
+            context = {"form": PostForm}
+            return render(request, "post_create.html", context)
+    def post(self, request):
         form = PostForm(request.POST)
         if form.is_valid():
             title = form.cleaned_data["title"]
@@ -28,14 +30,17 @@ def post_create_view(request):
 
         return redirect("posts")
 
-def post_view(request, post_id):
-    post = get_object_or_404(Post, id=post_id) #get은 데이터가 없으면 에러 반환, 장고에서 지원해주는 404 에러 처리 사용가능
-    context = {"post": post}
-    return render(request, "post_detail.html", context)
+class PostDetailView(View):
+    def get(self, request, post_id):
+        post = get_object_or_404(Post, id=post_id)  # get은 데이터가 없으면 에러 반환, 장고에서 지원해주는 404 에러 처리 사용가능
+        context = {"post": post}
+        return render(request, "post_detail.html", context)
 
-def post_like_view(request, post_id):
-    post = get_object_or_404(Post, id=post_id)
-    post.points = F("points") + 1 # post.points = F()로 들어가있음
-    post.save()
-    post.refresh_from_db() # DB에서 다시 불러오기
-    return render(request, "post_detail.html", {"post": post})
+class PostLikeView(View):
+    def post(self, request, post_id):
+        post = get_object_or_404(Post, id=post_id)
+        post.points = F("points") + 1 # post.points = F()로 들어가있음
+        post.save()
+        post.refresh_from_db() # DB에서 다시 불러오기
+        context = {"post": post}
+        return render(request, "post_detail.html", context)
