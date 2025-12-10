9b4af0c12bfc49fc031df8eb47e7a391c9269c62
ryudain05 <dkdlsekdls@naver.com>
2025-12-10 10:13:15 +0900

    feat: 로그인&로그아웃 화면 구현

--- ch3/auth_user/user/urls.py
+++ ch3/auth_user/user/urls.py
@@ -1,7 +1,7 @@
-from django.urls import path
+from django.urls import path, include
 from user import views
 
 urlpatterns = [
     path("", views.HomeView.as_view(), name="home"),
-    path("users/", include("django.contrib.auth.urls"))
+    path("users/", include("django.contrib.auth.urls")),
 ]
