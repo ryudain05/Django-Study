9401537091daba4c86ba3126c38dd305637c0649
ryudain05 <dkdlsekdls@naver.com>
2025-12-30 10:34:54 +0900

    feat: Django Field & Manager

--- ch5/hands_on/orm/models.py
+++ ch5/hands_on/orm/models.py
@@ -3,9 +3,14 @@ import uuid
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
+class CustomUserManager(models.Manager):
+    def with_image(self):
+        return super().get_queryset().filter(image__isnull=False)
+
 class CustomUser(AbstractUser):
     uuid = models.UUIDField(default=uuid.uuid4, editable=False, db_index=True)
     # media/profiles/image.png
     image = models.ImageField(upload_to='profiles/', null=True)
+    preferences = models.JSONField(default=dict)
 
-
+    objects = CustomUserManager()
