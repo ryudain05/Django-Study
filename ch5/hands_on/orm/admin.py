607e31b43888c692e00c9d2e26feb671426988bb
ryudain05 <dkdlsekdls@naver.com>
2026-01-09 10:39:36 +0900

    feat: django admin feat add

--- ch5/hands_on/orm/admin.py
+++ ch5/hands_on/orm/admin.py
@@ -1,3 +1,34 @@
 from django.contrib import admin
 
+from orm.models import CustomUser, Profile
+import csv
+from django.http import HttpResponse
+
+class ProfileInline(admin.StackedInline):
+    model = Profile
+    extra = 1
+
+def export_as_csv(modeladmin, request, queryset):
+
+    response = HttpResponse(content_type='text/csv')
+    response['Content-Disposition'] = 'attachment; filename="export.csv"'
+
+    writer = csv.writer(response)
+    field_names = [field.name for field in modeladmin.model._meta.fields]
+    writer.writerow(field_names)
+
+    for obj in queryset:
+        # ["admin", "admin@example...", "2024-01-01", ...]
+        row = [getattr(obj, field) for field in field_names]
+        writer.writerow(row)
+
+    return response
+
 # Register your models here.
+@admin.register(CustomUser)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ("username", "email", "date_joined")
+    search_fields = ("username", )
+    list_filter = ("is_staff", )
+    inlines = (ProfileInline, )
+    actions = [export_as_csv]
--- ch5/hands_on/orm/models.py
+++ ch5/hands_on/orm/models.py
@@ -1,16 +1,20 @@
 import uuid
 
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, UserManager
 
-class CustomUserManager(models.Manager):
+class CustomUserManager(UserManager):
     def with_image(self):
         return super().get_queryset().filter(image__isnull=False)
 
 class CustomUser(AbstractUser):
     uuid = models.UUIDField(default=uuid.uuid4, editable=False, db_index=True)
     # media/profiles/image.png
-    image = models.ImageField(upload_to='profiles/', null=True)
-    preferences = models.JSONField(default=dict)
+    image = models.ImageField(upload_to='profiles/', null=True, blank=True)
+    preferences = models.JSONField(default=dict, blank=True)
 
     objects = CustomUserManager()
+
+class Profile(models.Model):
+    user = models.OneToOneField(CustomUser, on_delete=models.CASCADE)
+    job = models.TextField(max_length=32, default="developer")
