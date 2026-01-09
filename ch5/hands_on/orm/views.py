96afd1d6cd8374685b6b0f9bc4cf1d77ca0522b6
ryudain05 <dkdlsekdls@naver.com>
2026-01-09 09:43:37 +0900

    feat: redis cache 활용

--- ch5/hands_on/orm/views.py
+++ ch5/hands_on/orm/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.views.decorators.cache import cache_page
 
 # Create your views here.
+@cache_page(60)
+def home_view(request):
+    print("오래 걸리는 작업 진행 중 ....")
+    return render(request, "email.html")
--- ch5/hands_on/utils.py
+++ ch5/hands_on/utils.py
@@ -1,6 +1,7 @@
 from django.core.mail import send_mail, EmailMessage
 from django.template.loader import render_to_string
 from django.conf import settings
+from django.core.cache import cache
 
 def send_str_email(email_to):
     send_mail(
@@ -11,6 +12,14 @@ def send_str_email(email_to):
     )
 
 def send_html_email(email_to):
+    email_html_cache_key = "email_html"
+
+
+    if not (html := cache_key.get(key=email_html_cache_key)):
+        print("새로운 html 파일을 읽는 중 ...")
+        html = render_to_string("email.html") # 오래 걸리는 작업
+        cache.set(key=email_html_cache_key, value=html)
+
     subject = "새로운 이메일이 도착했습니다."
     html = render_to_string("email.html") # 이메일을 읽어서 String 형태로 할당
     message = EmailMessage(
