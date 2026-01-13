0891f92983dfd6595882546ac3f3e60eed60b301
ryudain05 <dkdlsekdls@naver.com>
2026-01-13 11:15:08 +0900

    feat: GenericViewSet

--- ch4/short_url/shortener/api.py
+++ ch4/short_url/shortener/api.py
@@ -1,7 +1,9 @@
 from rest_framework import status
+from rest_framework.mixins import ListModelMixin, CreateModelMixin
 from rest_framework.generics import get_object_or_404, GenericAPIView, ListAPIView, ListCreateAPIView, DestroyAPIView
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework.viewsets import GenericViewSet
 
 from shortener.models import ShortURL
 from shortener.serializers import ShortURLResponseSerializer, ShortURLCreateSerializer
@@ -28,3 +30,24 @@ class ShortURLAPIView(ListCreateAPIView):
 class ShortURLDetailAPIView(DestroyAPIView):
     queryset = ShortURL.objects.all()
     lookup_field = "code"
+
+class ShortURLViewSet(
+    ListModelMixin, CreateModelMixin, DestroyAPIView,GenericViewSet
+):
+    queryset = ShortURL.objects.all()
+    serializer_class = ShortURLResponseSerializer
+    lookup_field = "code"
+
+    def perform_create(self, serializer):
+        while True:
+            code = ShortURL.generate_code()
+            if not ShortURL.objects.filter(code=code).exists():
+                break
+        serializer.save(code=code)
+
+    def create(self, request, *args, **kwargs):
+        serializer = ShortURLCreateSerializer(data=request.data)
+        if serializer.is_valid():
+            self.perform_create(serializer)
+            return Response(data=ShortURLResponseSerializer(serializer.instance).data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
--- ch4/short_url/shortener/urls.py
+++ ch4/short_url/shortener/urls.py
@@ -1,5 +1,14 @@
-from django.urls import path
-from shortener import views, api
+from django.urls import path, include
+from shortener import views
+from shortener.api import ShortURLViewSet
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+router.register(
+    "api/short-urls",
+    ShortURLViewSet,
+    "short_urls"
+)
 
 urlpatterns = [
     path("", views.HomeView.as_view(), name="home"),
@@ -7,6 +16,10 @@ urlpatterns = [
     path("<str:code>/", views.ShortURLDetailView.as_view(), name="short_url_detail"),
 
     # drf
-    path("api/short-urls/", api.ShortURLAPIView.as_view(), name="short_urls_api"),
-    path("api/short-urls/<str:code>/", api.ShortURLDetailAPIView.as_view(), name="short_url_detail_api"),
+    # path("api/short-urls/", api.ShortURLAPIView.as_view(), name="short_urls_api"),
+    # path("api/short-urls/<str:code>/", api.ShortURLDetailAPIView.as_view(), name="short_url_detail_api"),
+
+    # view set
+    path("", include(router.urls)),
+
 ]
