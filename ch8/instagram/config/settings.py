2f34d0dbe9fee4d0e4fa0876d0bfa1eb95d223df
ryudain05 <dkdlsekdls@naver.com>
2026-01-16 10:27:13 +0900

    init:docker-compose yml init

--- ch8/instagram/config/settings.py
+++ ch8/instagram/config/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 
 from pathlib import Path
 
+from instagram.user.models import CustomUser
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -27,6 +29,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+AUTH_USER_MODEL = "user.CustomUser"
 
 # Application definition
 
@@ -37,6 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'user.apps.UserConfig',
+    'feed.apps.FeedConfig',
 ]
 
 MIDDLEWARE = [
