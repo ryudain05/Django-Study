c3aec0ce0bde9ce157cfa97c8fb2948490000699
ryudain05 <dkdlsekdls@naver.com>
2026-01-20 11:32:22 +0900

    feat: pagination add

--- ch8/instagram/feed/views.py
+++ ch8/instagram/feed/views.py
@@ -1,11 +1,15 @@
 from django.shortcuts import render
 from rest_framework.generics import ListCreateAPIView
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.pagination import CursorPagination
 from rest_framework_simplejwt.authentication import JWTAuthentication
 
 from feed.models import Post
 from feed.serializers import PostSerializer
 
+class PostCursorPagination(CursorPagination):
+    page_size = 1
+    ordering = '-created_at'
 
 # Create your views here.
 class PostView(ListCreateAPIView):
@@ -13,6 +17,8 @@ class PostView(ListCreateAPIView):
     serializer_class = PostSerializer
     permission_classes = [IsAuthenticated]
     authentication_classes = [JWTAuthentication]
+    pagination_class = PostCursorPagination # 특정 API 뷰에서 커서 페이지네이션을 사용하도록 설정
 
     def perform_create(self, serializer):
         serializer.save(user_id=self.request.user.id)
+
