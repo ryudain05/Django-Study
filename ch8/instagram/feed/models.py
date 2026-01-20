5cdadd844da5113d87e0ff65c828a6e61e23a674
ryudain05 <dkdlsekdls@naver.com>
2026-01-20 10:00:41 +0900

    feat: PostComment, Post modeling

--- ch8/instagram/feed/models.py
+++ ch8/instagram/feed/models.py
@@ -1,3 +1,6 @@
+import os
+import uuid
+
 from django.db import models
 
 from user.models import CustomUser
@@ -5,9 +8,40 @@ from user.models import CustomUser
 
 # Create your models here.
 
+def unique_file_pate(instance, filename):
+    ext = filename.split('.')[-1]
+    filename = f"{uuid.uuid4()}.{ext}"
+    return os.path.join('posts/images/', filename)
+    # posts/images/<uuid>.<ext>
+
 # images/
 # a.png
 # b.png
 class Post(models.Model):
     user = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name="posts")
-    image = models.ImageField(upload_to="images")
+    image = models.ImageField(upload_to=unique_file_pate)
+    description = models.TextField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-created_at']
+
+class PostLike(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="likes")
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name="likes")
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(fields=['post', 'user'], name="unique_like_per_user")
+        ]
+
+class PostComment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments")
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name="post_comments")
+    parent = models.ForeignKey("self", null=True, on_delete=models.CASCADE, related_name="replies")
+    content = models.CharField(max_length=200)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-created_at']
